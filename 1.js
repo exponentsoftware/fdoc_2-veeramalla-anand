da22e7d035dc9097bbb1aa147639f4dde727c5aa
veeramalla-anand <anandveeramalla202@gmail.com>
2021-05-17 21:21:14 +0530

    assignment

---
+++ 1.js
@@ -0,0 +1,24 @@
+const countries = [
+    'ALBANIA',
+    'BOLIVIA',
+    'CANADA',
+    'DENMARK',
+    'ETHIOPIA',
+    'FINLAND',
+    'GERMANY',
+    'HUNGARY',
+    'IRELAND',
+    'JAPAN',
+    'KENYA'
+  ];
+      console.log(countries[0],countries[0].length,countries[0].substring(0,3));
+      console.log(countries[1],countries[1].length,countries[1].substring(0,3));
+      console.log(countries[2],countries[2].length,countries[2].substring(0,3));
+      console.log(countries[3],countries[3].length,countries[3].substring(0,3));
+      console.log(countries[4],countries[4].length,countries[4].substring(0,3));
+      console.log(countries[5],countries[5].length,countries[5].substring(0,3));
+      console.log(countries[6],countries[6].length,countries[6].substring(0,3));
+      console.log(countries[7],countries[7].length,countries[7].substring(0,3));
+      console.log(countries[8],countries[8].length,countries[8].substring(0,3));
+      console.log(countries[9],countries[9].length,countries[9].substring(0,3));
+      console.log(countries[10],countries[10].length,countries[10].substring(0,3));
